4a708caddb04cb3c55c746c7a7992d42697ab0f5
Pieter de Rijk <pieter@de-rijk.com>
2017-04-26 14:08:33 +0200

    - Added some more comments to the script/configuration file - Changed variabel NAME_SPOOL to SPOOL_PATH in the script

--- subject_alternative_names.sh
+++ subject_alternative_names.sh
@@ -6,20 +6,30 @@
 #        https://github.com/paderijk/httpd-ssl-smart-rewrite-based-on-cert
 #
 
+# Certificate file to use
 CERT_FILE=/etc/httpd/conf/ssl.crt/server.crt
-NAME_SPOOL="/var/spool/pki/san/"
+
+# Location of the SPOOL
+SPOOL_PATH="/var/spool/pki/san/"
+
+# Use current date so in the spool-dir you can see once it was updated
 CURRENT_DATE=$(date)
-if [ ! -d $NAME_SPOOL ];
+if [ ! -d $SPOOL_PATH ];
 then
-    echo "[I] Creating Spool file $NAME_SPOOL"
-    mkdir -p $NAME_SPOOL
+    echo "[I] Creating Spool file $SPOOL_PATH"
+    mkdir -p $SPOOL_PATH
 fi
 
+# Clean current content of the spool
+# so changes (additions, but especially deletion) 
+# are reflected correctly
 echo "[I] Clean spool content"
-rm -f $NAME_SPOOL/*-san
+rm -f $SPOOL_PATH/*-san
+
+# Populate the spool
 echo "[I] Populating spool content based in $CERT_FILE"
 for x in $(openssl x509 -text -noout -in $CERT_FILE | grep DNS | sed -e 's/DNS://g' -e 's/, / /g');
 do
     echo "  [I] \"$x\" is in certificate"
-    echo $CURRENT_DATE > $NAME_SPOOL/$x-san
+    echo $CURRENT_DATE > $SPOOL_PATH/$x-san
 done
